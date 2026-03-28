f30a18f60e4f50620bb2ec4663a2f067b60560ad
Taiwo Olapade <semiteprofessor@gmail.com>
2026-03-28 06:33:50 +0100

    Commit

--- src/main/java/model/Recommendation.java
+++ src/main/java/model/Recommendation.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -24,6 +25,6 @@ public class Recommendation {
 
     @CreatedDate
     private LocalDateTime createdAt;
-    @
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
