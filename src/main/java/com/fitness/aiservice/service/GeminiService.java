70269c879ada404248662396b8f662a582f6fd9d
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-03 08:10:46 +0100

    Commit

---
+++ src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -0,0 +1,10 @@
+package com.fitness.aiservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class GeminiService {
+
+    private final WebClient webClient;
+}
